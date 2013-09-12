6c8fce4af8d26cd67324ca6b7041fbc89996154f
Adam Preston <adam.m.preston@gmail.com>
2013-09-12 12:52:57 -0400

    base structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MleApp::Application.config.secret_token = '784f609dd32a439e7cfd1b31898889a3c20770b72628edad1bad43f662935d7cfe6d00d46c84d82b62b2f7e148d535cb8b4a4981ad6c9f3b0a3ac9e07cd53f62'
